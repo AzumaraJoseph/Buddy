1cc2558708298550c5a3c13acf50c7da9193c66a
Azumara Joseph <azumarajoseph@gmail.com>
2024-10-14 18:33:31 +0100

    Update code

--- src/Auth/UseAuth.js
+++ src/Auth/UseAuth.js
@@ -1,4 +1,3 @@
-// src/hooks/useAuth.js
 import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 
@@ -10,28 +9,21 @@ const useAuth = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        // Simulate fetching authentication status from local storage or an API
         const authStatus = localStorage.getItem('isAuthenticated');
         setIsAuthenticated(authStatus === 'true');
         setLoading(false);
     }, []);
 
     const login = (token) => {
-        // Save authentication token or status (e.g., token from an API response)
         localStorage.setItem('isAuthenticated', 'true');
         setIsAuthenticated(true);
     };
 
     const logout = () => {
-        // Clear authentication token or status
-        // localStorage.removeItem('isAuthenticated');
-        // setIsAuthenticated(false);
-
         // Clear any authentication data (like tokens) from localStorage
         localStorage.removeItem("token");
         localStorage.removeItem("user");
 
-        // Navigate to the login page
         navigate("/login");
     };
 
