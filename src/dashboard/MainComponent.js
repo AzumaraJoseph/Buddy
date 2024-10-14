02f3846591603b9dda28eeda258427995bf254bd
Azumara Joseph <azumarajoseph@gmail.com>
2024-10-14 15:05:38 +0100

    Update code

--- src/dashboard/MainComponent.js
+++ src/dashboard/MainComponent.js
@@ -24,7 +24,7 @@ const MainComponent = () => {
 
         <div className="main">
             <div className="top">
-                <h4>My Portfolio</h4>
+                <h4>My Portfoliosssssss</h4>
                 <div className="find">
                     {/* Input field with search icon inside */}
                     <div className="search-bar">
